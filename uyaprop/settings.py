38e1edfd1c4b8a744728407acec66406223ad937
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2023-05-01 16:44:17 +0100

    Created realestate app

--- uyaprop/settings.py
+++ uyaprop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'realestate.apps.RealestateConfig',
     'crispy_forms',
     'compressor',
 ]
