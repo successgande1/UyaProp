9cd7d8b27e03f471b7e6070ada158d18b7f07c73
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2023-05-08 17:51:25 +0100

    Rewrite account urls module code

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    
-    path('register/', views.account, name = 'register-account'),
-  
+    path('register/', views.register, name='register-account'),
+    path('success/', views.registration_success, name='success-account'),
 ]
