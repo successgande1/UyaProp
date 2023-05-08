6d1e2e320f230a037998182072ed6efc05d480f2
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2023-05-08 17:42:50 +0100

    Added the account app url paths

--- uyaprop/urls.py
+++ uyaprop/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('', include('account.urls')),
 ]+ static ( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
