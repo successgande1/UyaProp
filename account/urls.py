a7e36962faefb807f2702b453d97a24749d370fb
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2023-05-08 19:28:46 +0100

    added url path for account login

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('register/', views.register, name='register-account'),
+    path('', auth_view.LoginView.as_view(template_name='account/login.html'), name = 'account-login'),
     path('success/', views.registration_success, name='success-account'),
 ]
