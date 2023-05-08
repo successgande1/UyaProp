21194b5b8414dfdecae37e5c9991eee317c572a6
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2023-05-08 18:09:46 +0100

    Create account Model module with code

--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(default='avatar.jpg', blank=False, null=False, upload_to ='profile_images', 
+   
+    )
+    # Add any additional fields as needed
+
+    def __str__(self):
+        return self.user.username
