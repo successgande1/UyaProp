190fb6680269c30e188f8d85c3bdefac848ea542
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2023-05-09 11:36:08 +0100

    Registered Realestate app models

--- account/views.py
+++ account/views.py
@@ -3,24 +3,41 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login
 
 from . forms import *
+from realestate.models import *
 
 #Register New User Method
 def register(request):
     if request.method == 'POST':
-        form = CreateUserForm(request.POST) 
+        form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-           
-            return redirect('account-login')
+            # Create a new user object
+            user = form.save()
+            # Get the user_type value from the form
+            user_type = form.cleaned_data['user_type']
+            # Create a new object based on the user_type
+            if user_type == 'landlord':
+                Landlord.objects.create(user=user)
+            elif user_type == 'agent':
+                Agent.objects.create(user=user)
+            elif user_type == 'prospect':
+                Prospect.objects.create(user=user)
+            # Log the user in and redirect to the homepage
+            login(request, user)
+            return redirect('success-account')
     else:
-        form = CreateUserForm()
-    context = {
-        'form':form,
-        'page_title':'Register',
-    }
+        form = RegistrationForm()
+    context =  {
+        'form': form,
+        'page_title': 'Register',
+        }
     return render(request, 'account/register.html', context)
 
 def registration_success(request):
-    return render(request, 'account/update_profile.html')
+    context =  {
+        
+        'page_title': 'Update Profile',
+        }
+    return render(request, 'account/update_profile.html', context)
--- realestate/admin.py
+++ realestate/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+admin.site.register(Landlord)
+
+admin.site.register(Agent)
+
+admin.site.register(Prospect)
+
+admin.site.register(Tenancy)
