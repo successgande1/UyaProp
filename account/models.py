deb6bf2c195833e5d4e02ee5584a04573e4d22b2
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2023-05-11 07:46:27 +0100

    Removed user_type field from profile model

--- account/models.py
+++ account/models.py
@@ -4,7 +4,6 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=20, blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(default='avatar.jpg', blank=False, null=False, upload_to ='profile_images', 
